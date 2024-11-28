f260e331cd82277da7ba927958357a4124661b55
mochamadgalih <mochamadglh@gmail.com>
2024-11-29 01:33:35 +0700

    initial upload project

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+import Header from "./components/Header.jsx";
+import Result from "./components/Result.jsx";
+import UserInput from "./components/UserInput.jsx";
+function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  const inputIsValid = userInput.duration <= 0;
+  const handleChange = (inputIdentifier, newValue) => {
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        [inputIdentifier]: +newValue,
+      };
+    });
+  };
+  return (
+    <>
+      <Header />
+      <UserInput userInput={userInput} onChange={handleChange} />
+      {inputIsValid && (
+        <p className="center">Please enter a duration grater than 0.</p>
+      )}
+      {!inputIsValid && <Result input={userInput} />}
+    </>
+  );
+}
+
+export default App;
